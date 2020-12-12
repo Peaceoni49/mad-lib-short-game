bcc88f59e95a8727ef376b7febdee65c239023ee
Peaceoni49 <47338298+Peaceoni49@users.noreply.github.com>
2020-12-12 22:24:38 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome to the Mad-Lib game where you come up with words to fill in the story.")
+print("Our story today is : 'The ___ ___ ___ over the tall fence'")
+print("Let'sfinish this story...")
+print("\n")
+
+adjective = input("Enter an adjective: ")
+noun = input ("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print("\n")
+print("YOUR MAD-LIB STORY")
+print("\n")
+print("The "+ adjective + " "+ noun +" " + verb +" "+ "over the tall fence")
+
